093bcd573944a2ac04b1c8a8446289b01579f1a2
Dustin Specker <dustinspecker@dustinspecker.com>
2015-05-23 19:21:59 -0500

    initial commit

---
+++ server.ts
@@ -0,0 +1,24 @@
+import Hapi = require('hapi');
+
+let server = new Hapi.Server();
+server.connection({port: 3000});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler(request: Hapi.Request, reply: Hapi.IReply) {
+    reply('Hello, world!');
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/{name}',
+  handler(request: Hapi.Request, reply: Hapi.IReply) {
+    reply(`Hello, ${encodeURIComponent(request.params['name'])}`);
+  }
+});
+
+server.start(() => {
+  console.log(`Server running at ${server.info.uri}`);
+});
